8b92f7e9798363779a67288d0bbc3fc17d788921
Doug <clarke.doug248@gmail.com>
2016-05-22 15:10:04 -0400

    rendering one enemy

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(yPosition) {
     this.sprite = 'images/enemy-bug.png';
+    this.x=0;
+    this.y=yPosition;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
+xPositions = []
+numCols = 5
+for (col = 0; col < numCols; col++) {
+  xPositions.push(col * 101);
+}
+
 Enemy.prototype.update = function(dt) {
+    this.x = xPositions[xPositions.indexOf(this.x)+1];
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -21,11 +25,24 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var player = function(){
+  this.sprite = 'images/char-boy.png';
+}
+player.prototype = Object.create(Enemy.prototype);
+player.prototype.constructor = player;
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+enemyYpositions = [];
+var tileHeight = 83;
+enemyYpositions.push(tileHeight-20,tileHeight*2-20,tileHeight*3-20);
 
+var allEnemies = []
+allEnemies.push(new Enemy(enemyYpositions[Math.floor(Math.random() * enemyYpositions.length)]));
+
+var player = new player;
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
